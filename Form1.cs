651de744fd5af9dcc213c99b7aed6482391a57f1
Florent Imeri <76780812+Florent115@users.noreply.github.com>
2022-01-28 20:38:45 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Florent" && textBox2.Text == "Florent1")
+            {
+                Form2 frm = new Form2();
+                frm.Show();
+            }
+            else
+            {
+                label3.Text = "Incorrect Credentials";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Cut();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.SelectAll();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.richTextBox1.CanUndo)
+                this.richTextBox1.Undo();
+        }
+
+        private void fileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Title = "Hap File-in";
+            opf.Filter = "Text Document|*.txt";
+
+            if(opf.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(opf.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Save File";
+            saveFileDialog1.OverwritePrompt = true;
+
+            if(saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.printDialog1.Document = this.printDocument1;
+
+            if(this.printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.printDocument1.Print();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.fontDialog1.ShowColor = true;
+            this.fontDialog1.ShowDialog();
+            this.richTextBox1.Font = this.fontDialog1.Font;
+            this.richTextBox1.ForeColor = this.fontDialog1.Color;
+           
+        }
+    }
+}
